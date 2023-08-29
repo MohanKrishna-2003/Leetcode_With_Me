aa9dbc8e94fe808420cf6ff0fd1914875e40ca62
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-08-29 20:09:34 +0530

    Time: 1 ms (41.22%), Space: 39.1 MB (61.34%) - LeetHub

---
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int n = Math.max(a, b), count = 0;
+        for(int i=1; i<=n; i++)
+        {
+            if(a%i==0 && b%i==0)
+                count++;
+        }
+        return count;
+    }
+}
