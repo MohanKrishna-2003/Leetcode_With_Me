4eacb722b77aa92f100d658b137d097b72ac833b
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-10-11 15:18:09 +0530

    Time: 1 ms (91.99%), Space: 39.4 MB (59.48%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,16 +1,15 @@
 class Solution {
-    public boolean isUgly(int n) {
-        if (n <= 0) {
+    public boolean isUgly(int n) 
+    {
+        if(n<=0)
             return false;
-        }
-        if (n == 1) {
+        if(n==1)
             return true;
-        }
-        for (int i = 2; i <= 5; i++) {
-            while (n % i == 0) {
-                n /= i;
-            }
-        }
-        return n == 1;
+        for(int i=2; i<=5; i++)
+        {
+            while(n%i==0)
+                n/=i;
+        }    
+        return n==1;
     }
 }
