f64cf4eefbc9c3fb56d9af6c0ad24c593b6971d6
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-10-12 19:42:46 +0530

    Time: 0 ms (100%), Space: 39 MB (58.79%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        int arr[] = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i=2; i<n; i++)
+        {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n-1];
+    }
+}
