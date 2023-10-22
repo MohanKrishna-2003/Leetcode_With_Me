906ba182ff4b30c7b7278f84164d8867472931ef
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-10-22 12:17:19 +0530

    Time: 0 ms (100%), Space: 43.6 MB (55.46%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int n = indices.length;
+        char ch[] = new char[n];
+        for(int i=0; i<n; i++)
+        {
+            ch[indices[i]] = s.charAt(i);
+        }
+        return new String(ch);
+
+    }
+}
